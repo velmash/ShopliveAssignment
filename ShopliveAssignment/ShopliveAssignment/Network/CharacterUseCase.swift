0c55c3a9edf1c5598b1631447a1d2e1954179b5c
velmash <hyengchan@gmail.com>
2024-10-13 22:08:35 +0900

    :wrench: API 호출 로직 구현

---
+++ ShopliveAssignment/ShopliveAssignment/Network/CharacterUseCase.swift
@@ -0,0 +1,29 @@
+//
+//  CharacterUseCase.swift
+//  ShopliveAssignment
+//
+//  Created by 윤형찬 on 10/13/24.
+//
+
+import Foundation
+
+class MarvelAPI {
+    static let shared = MarvelAPI()
+    private let client = MarvelAPIClient()
+    
+    private init() {}
+    
+    func searchCharacters(nameStartsWith: String, completion: @escaping (Result<[Character], Error>) -> Void) {
+        let endpoint = "/characters"
+        let parameters = ["nameStartsWith": nameStartsWith]
+        
+        client.run(endpoint: endpoint, parameters: parameters) { (result: Result<MarvelResponse<Character>, Error>) in
+            switch result {
+            case .success(let response):
+                completion(.success(response.data.results))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
