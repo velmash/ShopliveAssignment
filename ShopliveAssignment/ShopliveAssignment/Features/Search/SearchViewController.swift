7c89525bb7f4d0fbebb18d75de274bf0b539de1b
velmash <hyengchan@gmail.com>
2024-10-13 23:00:32 +0900

    :wrench: 검색 후 0.3초 뒤 2자 이상일 때 API 호출 > 받아내는 로직 구현

--- ShopliveAssignment/ShopliveAssignment/Features/Search/SearchViewController.swift
+++ ShopliveAssignment/ShopliveAssignment/Features/Search/SearchViewController.swift
@@ -12,8 +12,28 @@ class SearchViewController: BaseViewController<SearchView> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        viewModel?.searchAndPrintCharacters(nameStartsWith: "iron")
+        
+        contentView.searchBar.delegate = self
     }
 }
 
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        contentView.searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.count >= 2 {
+            viewModel?.searchCharacters(nameStartsWith: searchText) { [weak self] characters in
+                // UI 업데이트 로직
+                self?.updateUI(with: characters)
+            }
+        }
+    }
+}
+
+extension SearchViewController {
+    func updateUI(with characters: [Character]) {
+        print("캐릭터들", characters)
+    }
+}
--- ShopliveAssignment/ShopliveAssignment/Features/Search/Views/SearchView.swift
+++ ShopliveAssignment/ShopliveAssignment/Features/Search/Views/SearchView.swift
@@ -6,15 +6,36 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 final class SearchView: BaseView {
+    lazy var searchBar = UISearchBar().then {
+        $0.placeholder = "마블 영웅 이름을 입력하시오."
+        $0.backgroundImage = UIImage()
+        $0.backgroundColor = .clear
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.backgroundColor = .yellow
+        self.backgroundColor = .white
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+    }
+    
+    override func addSubviews() {
+        addSubview(searchBar)
+    }
+    
+    override func addConstraints() {
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(16)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(50)
+        }
     }
 }
